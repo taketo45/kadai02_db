cb4497a4a86bdc93578c6dc59b082a84f44b5988
takezo <taketo45@gmail.com>
2024-11-27 21:50:56 +0900

    localhost Issue Cleare

--- selectAll.php
+++ selectAll.php
@@ -1,27 +1,29 @@
 <?php
 require_once('../../appconfig/config.php');
 
-require_once('lib/DatabaseInfo.php');
+// require_once('lib/DatabaseInfo.php');
 require_once('lib/DatabaseController.php');
 
 //エラー表示
 ini_set("display_errors", 1);
 $isDebug = true;
 
-
+$db = null;
 //データ取得
-$dbinfo = new DatabaseInfo();
+// $dbinfo = new DatabaseInfo(); //不具合確認中
 
 //データ取得
 try {
-  $db = new DatabaseController($dbinfo->dbName, $isDebug, $dbinfo->host, $dbinfo->user, $dbinfo->pass);
+  // $db = new DatabaseController($dbinfo->dbName, $isDebug, $dbinfo->host, $dbinfo->user, $dbinfo->pass);
+  $db = new DatabaseController(PRO_DB_NAME, $isDebug, PRO_DB_HOST, PRO_DB_USER, PRO_DB_PASS);
+  // $db = new DatabaseController('english_study', $isDebug);
 
 } catch (Exception $e) {
-  if($dbinfo->is_server){
+  // if($dbinfo->is_server){
+  //   error_log( $e->getMessage() );
+  // } else {
     error_log( $e->getMessage() );
-  } else {
-    error_log( $e->getMessage() );
-  }
+  // }
 }
 
 // $db = new DatabaseController('english_study', $isDebug);
